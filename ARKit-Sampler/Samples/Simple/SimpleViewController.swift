c1efe71a326aedee2edfe491b185a91e316a63fc
hijazi (mac studio) <hijazi@irekasoft.com>
2025-02-13 22:32:14 +0800

    adjust appdelegate.

--- ARKit-Sampler/Samples/Simple/SimpleViewController.swift
+++ ARKit-Sampler/Samples/Simple/SimpleViewController.swift
@@ -10,21 +10,43 @@ import UIKit
 import ARKit
 
 class SimpleViewController: UIViewController {
-
-    @IBOutlet var sceneView: ARSCNView!
+  
+  var modelURL: URL?
+  
+  var sceneView: ARSCNView!
+  
+  override func viewDidLoad() {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()        
-        sceneView.scene = SCNScene(named: "ship.scn", inDirectory: "models.scnassets/ship")!
+    super.viewDidLoad()
+    
+    // Create and setup sceneView
+    sceneView = ARSCNView(frame: view.bounds)
+    view.addSubview(sceneView)
+    
+    // Load scene
+    if let modelURL = modelURL {
+      sceneView.allowsCameraControl = true
+      sceneView.scene = SCNScene(named: modelURL.lastPathComponent, inDirectory: modelURL.deletingLastPathComponent().path)!
+    } else {
+      print("No model URL provided")
+      
+      let alert = UIAlertController(title: "Error", message: "No model URL provided", preferredStyle: .alert)
+      alert.addAction(UIAlertAction(title: "OK", style: .default))
+      present(alert, animated: true)
+      
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        sceneView.session.run(ARWorldTrackingConfiguration())
-    }
+    // sceneView.scene = SCNScene(named: "ship.scn", inDirectory: "models.scnassets/ship")!
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        sceneView.session.pause()
-    }
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    sceneView.session.run(ARWorldTrackingConfiguration())
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    sceneView.session.pause()
+  }
 }
